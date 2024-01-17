16f745f8520b3f8ba1617f35931d41f656647260
Renan Luiz Vendramini <renanlvendramini@gmail.com>
2024-01-17 15:15:36 -0300

    Refactor URLParserTest class

--- lib/URLParser.php
+++ lib/URLParser.php
@@ -4,111 +4,153 @@
  * A class to parse an URL from its string, modify the parts independently
  * and then turn it to a string again.
  * Works like the URL class from Javascript.
- * 
+ *
  * @author Renan Luiz Vendramini <renanlvendramini@gmail.com>
  * @version 1.0
  */
-final class URLParser {
-  protected $originalUrl;
+final class URLParser
+{
+    protected readonly string $originalUrl;
 
-  private $parsedUrl;
+    private readonly array $parsedUrl;
 
-  private $protocol = null;
-  private $host = null;
-  private $path = null;
-  private $query = null;
-  private $fragment = null;
-  private $queryParams = [];
+    private ?string $protocol;
+    private ?string $host;
+    private ?string $path;
+    private ?string $query;
+    private ?string $fragment;
+    private array $queryParams;
 
-  const DEFAULT_PROTOCOL = 'https';
-  const DEFAULT_PATH = '/';
+    public const DEFAULT_PROTOCOL = 'https';
+    public const DEFAULT_PATH = '/';
+    public const PARTS = ['scheme', 'host', 'path', 'query', 'fragment'];
 
-  function __construct($url) {
-    $this->validateUrl($url);
-    
-    $this->originalUrl = $url;
-    $this->destructure();
-  }
+    public function __construct(private readonly string $url)
+    {
+        $this->validateUrl($url);
 
-  // public functions
+        $this->originalUrl = $url;
+        $this->protocol = $this::DEFAULT_PROTOCOL;
+        $this->path = $this::DEFAULT_PATH;
 
-  public static function fromString($url) {
-    return new self($url);
-  }
-
-  public function getParam($key) {
-    if (!isset($this->queryParams[$key])) return;
-
-    return $this->queryParams[$key];
-  }
-
-  public function setParam($key, $value) {
-    $key = $this->sanitizeKeyString($key);
-    $value = $this->sanitizeValueString($value);
-
-    $this->queryParams[$key] = $value;
-    return $this->queryParams[$key];
-  }
-
-  public function toString() {
-    $this->query = http_build_query($this->queryParams);
-    if ($this->query) $this->query = "?{$this->query}";
-
-    if (!$this->protocol) $this->protocol = $this::DEFAULT_PROTOCOL;
-    if (!$this->path) $this->path = $this::DEFAULT_PATH;
-
-    return "{$this->protocol}://{$this->host}{$this->path}{$this->query}{$this->fragment}";
-  }
-
-  // Private Functions
-
-  private function destructure() {
-    $this->parsedUrl = parse_url($this->originalUrl);
-    if (!$this->parsedUrl) return;
-
-    foreach(['scheme', 'host', 'path', 'query', 'fragment'] as $key) {
-      $this->$key = isset($this->parsedUrl[$key]) ? $this->parsedUrl[$key] : null;
+        $this->destructure();
     }
 
-    $this->parseQuery();
-  }
+    // public functions
 
-  private function parseQuery() {
-    if (!$this->query) return;
+    // static functions
 
-    foreach(explode("&", $this->query) as $term) {
-      $part = explode("=", $term);
-      if (!isset($part[0])) continue;
-
-      $sanitizedKey = $this->sanitizeKeyString($part[0]);
-      $sanitizedValue = $this->sanitizeValueString(isset($part[1]) ? $part[1] : '');
-
-      $this->queryParams[$sanitizedKey] = $sanitizedValue;
+    public static function fromString(string $url)
+    {
+        return new self($url);
     }
-  }
 
-  private function sanitizeKeyString($string) {
-    // turn into low caps
-    $str = trim(strtolower($string));
-    // turn \s and - into _
-    $str = preg_replace('/\s|\-/', '_', $str);
-    if (!$str) throw new Exception("'{$str}' is an invalid key");
-    // remove any character except alphanumerical and underscore
-    $str = preg_replace('/[^a-zA-Z0-9_]/', '', $str);
-    if (!$str) throw new Exception("'{$string}' is an invalid key");
+    // instance functions
 
-    return $str;
-  }
+    public function getParam(string $key): string | null
+    {
+        if (!isset($this->queryParams[$key])) {
+            return null;
+        }
 
-  private function sanitizeValueString($string) {
-    if (!$string) return;
+        return $this->queryParams[$key];
+    }
 
-    $str = trim(urlencode($string));
-    return $str;
-  }
+    public function setParam(string $key, string $value): string
+    {
+        $key = $this->sanitizeKeyString($key);
+        $value = $this->sanitizeValueString($value);
 
-  private function validateUrl($url) {
-    if (!filter_var($url, FILTER_VALIDATE_URL)) 
-      throw new InvalidArgumentException("'{$url}' is not a valid URL");
-  }
+        $this->queryParams[$key] = $value;
+        return $this->queryParams[$key];
+    }
+
+    public function removeParam(string $key): void
+    {
+        unset($this->queryParams[$key]);
+    }
+
+    public function getParams(): array
+    {
+        return $this->queryParams;
+    }
+
+    public function toString(): string
+    {
+        $this->query = http_build_query($this->queryParams);
+        if ($this->query) {
+            $this->query = "?{$this->query}";
+        }
+
+        return "{$this->protocol}://{$this->host}{$this->path}{$this->query}{$this->fragment}";
+    }
+
+    // Private Functions
+
+    private function destructure(): void
+    {
+        $this->parsedUrl = parse_url($this->originalUrl);
+        if (!$this->parsedUrl) {
+            return;
+        }
+
+        foreach($this::PARTS as $key) {
+            $this->$key = $this->parsedUrl[$key] ?? null;
+        }
+
+        $this->parseQuery();
+    }
+
+    private function parseQuery(): void
+    {
+        if (!$this->query) {
+            return;
+        }
+
+        foreach(explode("&", $this->query) as $term) {
+            [$key, $value] = explode("=", $term);
+            if (!isset($key)) {
+                continue;
+            }
+
+            $sanitizedKey = $this->sanitizeKeyString($key);
+            $sanitizedValue = $this->sanitizeValueString($value ?? '');
+
+            $this->queryParams[$sanitizedKey] = $sanitizedValue;
+        }
+    }
+
+    private function sanitizeKeyString(string $string): string
+    {
+        // remove leading and trailing spaces
+        $str = trim($string);
+        // turn into low caps
+        $str = strtolower($str);
+        // turn \s and - into _
+        $str = preg_replace('/\s|\-/', '_', $str);
+        if (!$str) {
+            throw new Exception("'{$str}' is an invalid key");
+        }
+
+        // remove any character except alphanumerical and underscore
+        $str = preg_replace('/[^a-zA-Z0-9_]/', '', $str);
+        if (!$str) {
+            throw new Exception("'{$string}' is an invalid key");
+        }
+
+        return $str;
+    }
+
+    private function sanitizeValueString(string $string): string
+    {
+        $str = trim(urlencode($string));
+        return $str;
+    }
+
+    private function validateUrl(string $url): void
+    {
+        if (!filter_var($url, FILTER_VALIDATE_URL)) {
+            throw new InvalidArgumentException("'{$url}' is not a valid URL");
+        }
+    }
 }
--- tests/URLParserTest.php
+++ tests/URLParserTest.php
@@ -6,120 +6,155 @@ use PHPUnit\Framework\TestCase;
 require __DIR__ . '/../lib/URLParser.php';
 
 
-class PHPUnitUtil {
-  public static function callMethod($obj, $name, $args) {
-    $class = new \ReflectionClass($obj);
-    $method = $class->getMethod($name);
-    $method->setAccessible(true);
-    return $method->invokeArgs($obj, $args);
-  }
+class PHPUnitUtil
+{
+    public static function callMethod($obj, $name, $args)
+    {
+        $class = new \ReflectionClass($obj);
+        $method = $class->getMethod($name);
+        $method->setAccessible(true);
+        return $method->invokeArgs($obj, $args);
+    }
 }
 
-final class URLParserTest extends TestCase {
-  public function testCanBeCreatedFromValidUrl() {
-    $this->assertInstanceOf(
-      URLParser::class,
-      URLParser::fromString('https://foo.bar')
-    );
-  }
+final class URLParserTest extends TestCase
+{
+    public function testCanBeCreatedFromValidUrl()
+    {
+        $this->assertInstanceOf(
+            URLParser::class,
+            URLParser::fromString('https://foo.bar')
+        );
+    }
 
-  public function testCannotBeCreatedFromInvalidUrl() {
-    $this->expectException(InvalidArgumentException::class);
+    public function testCannotBeCreatedFromInvalidUrl()
+    {
+        $this->expectException(InvalidArgumentException::class);
 
-    URLParser::fromString('invalid');
-  }
+        URLParser::fromString('invalid');
+    }
 
-  public function testCanBeUsedAsString() {
-    $instance = URLParser::fromString('https://foo.bar/?foo=bar');
+    public function testCanBeUsedAsString()
+    {
+        $instance = URLParser::fromString('https://foo.bar/?foo=bar');
 
-    $this->assertEquals(
-      'https://foo.bar/?foo=bar',
-      $instance->toString()
-    );
-  }
+        $this->assertEquals(
+            'https://foo.bar/?foo=bar',
+            $instance->toString()
+        );
+    }
 
-  public function testCanGetQueryStringValue() {
-    $instance = URLParser::fromString('https://foo.bar/?foo=bar');
+    public function testCanGetQueryStringValue()
+    {
+        $instance = URLParser::fromString('https://foo.bar/?foo=bar');
 
-    $this->assertEquals(
-      'bar',
-      $instance->getParam('foo')
-    );
-  }
+        $this->assertEquals(
+            'bar',
+            $instance->getParam('foo')
+        );
+    }
 
-  public function testCanModifyQueryStringValue() {
-    $instance = URLParser::fromString('https://foo.bar/?foo=bar');
-    $instance->setParam('foo', 'foobar');
+    public function testCanModifyQueryStringValue()
+    {
+        $instance = URLParser::fromString('https://foo.bar/?foo=bar');
+        $instance->setParam('foo', 'foobar');
 
-    $this->assertNotEquals(
-      'bar',
-      $instance->getParam('foo')
-    );
+        $this->assertNotEquals(
+            'bar',
+            $instance->getParam('foo')
+        );
 
-    $this->assertEquals(
-      'foobar',
-      $instance->getParam('foo')
-    );
-  }
+        $this->assertEquals(
+            'foobar',
+            $instance->getParam('foo')
+        );
+    }
 
-  public function testCanSetNewQueryStringValue() {
-    $instance = URLParser::fromString('https://foo.bar/?foo=bar');
-    $instance->setParam('bar', 'foo');
+    public function testCanSetNewQueryStringValue()
+    {
+        $instance = URLParser::fromString('https://foo.bar/?foo=bar');
+        $instance->setParam('bar', 'foo');
 
-    $this->assertEquals(
-      'foo',
-      $instance->getParam('bar')
-    );
-  }
-
-
-  public function testCanSetParamWithDirtyKeyAndValue() {
-    $instance = URLParser::fromString('https://foo.bar/');
-    $instance->setParam(' #Mega Foo~ ', 'foo bar');
-
-    $this->AssertNull(
-      $instance->getParam(' #Mega Foo~ ')
-    );
-
-    $this->assertEquals(
-      'foo+bar',
-      $instance->getParam('mega_foo')
-    );
-
-    $this->assertNotEquals(
-      'foo bar',
-      $instance->getParam('mega_foo')
-    );
-  }
-
-  public function testCanSanitizeKeyString() {
-    $instance = URLParser::fromString('https://foo.bar');
-
-    $returnVal = PHPUnitUtil::callMethod(
-      $instance,
-      'sanitizeKeyString', 
-      array(" #Mega Foo-bar~ ")
-    );
-
-    $this->assertEquals(
-      'mega_foo_bar',
-      $returnVal
-    );
-  }
+        $this->assertEquals(
+            'foo',
+            $instance->getParam('bar')
+        );
+    }
 
 
-  public function testCanSanitizeValueString() {
-    $instance = URLParser::fromString('https://foo.bar');
-    
-    $returnVal = PHPUnitUtil::callMethod(
-      $instance,
-      'sanitizeValueString', 
-      array("Foo Bar")
-    );
+    public function testCanSetParamWithDirtyKeyAndValue()
+    {
+        $instance = URLParser::fromString('https://foo.bar/');
+        $instance->setParam(' #Mega Foo~ ', 'foo bar');
 
-    $this->assertEquals(
-      'Foo+Bar',
-      $returnVal
-    );
-  }
+        $this->AssertNull(
+            $instance->getParam(' #Mega Foo~ ')
+        );
+
+        $this->assertEquals(
+            'foo+bar',
+            $instance->getParam('mega_foo')
+        );
+
+        $this->assertNotEquals(
+            'foo bar',
+            $instance->getParam('mega_foo')
+        );
+    }
+
+    public function testCanSanitizeKeyString()
+    {
+        $instance = URLParser::fromString('https://foo.bar');
+
+        $returnVal = PHPUnitUtil::callMethod(
+            $instance,
+            'sanitizeKeyString',
+            array(" #Mega Foo-bar~ ")
+        );
+
+        $this->assertEquals(
+            'mega_foo_bar',
+            $returnVal
+        );
+    }
+
+
+    public function testCanSanitizeValueString()
+    {
+        $instance = URLParser::fromString('https://foo.bar');
+
+        $returnVal = PHPUnitUtil::callMethod(
+            $instance,
+            'sanitizeValueString',
+            array("Foo Bar")
+        );
+
+        $this->assertEquals(
+            'Foo+Bar',
+            $returnVal
+        );
+    }
+
+    public function testCanRemoveParam()
+    {
+        $instance = URLParser::fromString('https://foo.bar/?foo=bar');
+        $instance->removeParam('foo');
+
+        $this->assertNull(
+            $instance->getParam('foo')
+        );
+    }
+
+    public function testCanGetParams()
+    {
+        $instance = URLParser::fromString('https://foo.bar/?foo=bar&bar=foo');
+
+        $this->assertEquals(
+            array(
+            'foo' => 'bar',
+            'bar' => 'foo'
+      ),
+            $instance->getParams()
+        );
+    }
 }
